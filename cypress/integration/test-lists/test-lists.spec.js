8225ecf1e90df3107fb3258b84bdd42c24c94c9f
azizarozi <aziza.rozi@giantmachines.com>
2022-04-21 18:02:37 -0400

    solutions for list

--- cypress/integration/test-lists/test-lists.spec.js
+++ cypress/integration/test-lists/test-lists.spec.js
@@ -2,7 +2,19 @@ Given('I visit the site localhost', () => {
     cy.visit('http://localhost:3000/');
     cy.wait(1000) // wait for page to load
 });
-
 When('I add an item to the todo list', () => {
-    cy.get('#add-todo').type('walk dog{enter}')
+  cy.get('#add-todo').click().type('walk dog{enter}');
 });
+Then('The item I added should appear in the list', () => {
+    cy.get('section').get('ul').get('#todo-list').get('label').contains('walk dog').should('be.visible');
+});
+
+Given('I click a list item to mark it as complete', () => {
+cy.get('div').get('input').get('[type=checkbox]').check()
+});
+Then('The item should have the class completed', () => {
+    cy.get('section').get('li').get('[class="todo completed"]')
+    });
+
+
+
