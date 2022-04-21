2e6ec3b40a4885f8120ccbac32c648014a2f5fee
nicoleparisis <nicole.parisis@giantmachines.com>
2022-04-21 16:20:51 -0400

    write next step function

--- cypress/integration/test-lists/test-lists.spec.js
+++ cypress/integration/test-lists/test-lists.spec.js
@@ -1,3 +1,8 @@
 Given('I visit the site localhost', () => {
     cy.visit('http://localhost:3000/');
+    cy.wait(1000) // wait for page to load
+});
+
+When('I add an item to the todo list', () => {
+    // code your next step here
 });
--- cypress/integration/test-login/test-login.spec.js
+++ cypress/integration/test-login/test-login.spec.js
@@ -1,3 +1,8 @@
 Given('I visit the site localhost', () => {
     cy.visit('http://localhost:3000/login');
+    cy.wait(1000) // wait for page to load
+});
+
+When('I type in a random username and password', () => {
+    // code your next step here
 });
