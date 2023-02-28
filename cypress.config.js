51d14decc70d47614d9b8335941fe0bab1e58903
azizarozi <aziza.rozi@giantmachines.com>
2023-02-28 15:15:30 -0500

    latest1528

--- cypress.config.js
+++ cypress.config.js
@@ -4,17 +4,20 @@ module.exports = defineConfig({
   viewportWidth: 600,
   viewportHeight: 800,
   defaultCommandTimeout: 4000,
-  e2e: {
+  // e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-    },
+    // setupNodeEvents(on, config) {
+    //   return require('./cypress/plugins/index.js')(on, config)
+    // },
     e2e: {
       baseUrl: 'http://localhost:3000',
       // specPattern: ["**/*.feature", "cypress/e2e/BDD/**/*.cy.{js,jsx,ts,tsx}"]
   },
   
-  },
-})
+  })
+
+
+
+
 
